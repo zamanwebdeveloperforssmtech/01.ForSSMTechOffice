283dceade42b0e12174b937e99b4d99ea3633cba
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2019-03-24 17:28:38 +0600

    All Working File Transfer to htdocs for Boss

---
+++ TeachingService/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>This is title</title>
+</head>
+<body>
+	This is body.
+	<h1>H1 Tag</h1>
+	<h2>Tag</h2>
+	<!--
+	<marquee direction="down">This is Marquee of Down Direction</marquee>
+	-->
+	<br />
+	<br />
+	<br />
+	<marquee behavior ="scroll" direction="up">This is Marquee of Up Direction. <br />This is Marquee of Up Direction. <br />This is Marquee of Up Direction. <br /></marquee>
+
+	</body>
+</html>
